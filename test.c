eeb5ee9a52e60ae2f444e631f8a98123e4863e7b
halflife-zyan <2548970667@qq.com>
2025-11-09 19:52:00 +0800

    second save

---
+++ test.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+#include <math.h>
+int a[10000];
+int b[10000];
+int main() {
+int m,n;
+scanf("%d %d",&m,&n);
+for(int i=1;i<=m;i++){
+    a[i]=getchar();
+    putchar(a[i]);
+}
+for(int j=1;j<=n;j++){
+    b[j]=getchar();
+}
+
+}
