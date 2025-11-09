6efe7ce47a24e33a6e0e84a430699b03b57fc898
halflife-zyan <2548970667@qq.com>
2025-11-09 20:45:19 +0800

    okma

--- test.c
+++ test.c
@@ -3,14 +3,6 @@
 int a[10000];
 int b[10000];
 int main() {
-int m,n;
-scanf("%d %d",&m,&n);
-for(int i=1;i<=m;i++){
-    a[i]=getchar();
-    putchar(a[i]);
-}
-for(int j=1;j<=n;j++){
-    b[j]=getchar();
-}
+printf("helloworld");
 
 }
