af0d607162ec493bfa2b1fc8e05a59eee88eecfc
halflife-zyan <2548970667@qq.com>
2025-11-10 23:51:50 +0800

    save4

---
+++ noob69.cpp
@@ -0,0 +1,28 @@
+#include <bits/stdc++.h>
+using namespace std;
+/*struct Student{
+    string name;
+    int ch,ma,en,tot;
+    Student(string A,int B,int C,int D){
+        name=A;ch=B;ma=C;en=D;tot=B+C+D;
+    }
+    Student()=default;
+    bool operator > (const Student &x)const{
+        return tot>x.tot;
+    }
+}a[2000];
+int main(){
+    int n;
+    string A;
+    int B,C,D;
+    cin>>n;
+    for(int i=1;i<=n;i++){
+        cin>>A>>B>>C>>D;
+        a[i]=Student(A,B,C,D);
+    }
+    Student ans=a[1];
+    for(int i=2;i<=n;i++){
+        if(a[i]>ans) ans=a[i];
+    }
+    cout<<ans.name<<" "<<ans.ch<<" "<<ans.ma<<" "<<ans.en;
+}*/
